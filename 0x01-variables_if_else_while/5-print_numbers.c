2d520f74d6b16daaff5fa8bb2ca2f9330548bf1c
tob-serve <leinaddaniel2800@gmail.com>
2023-07-28 16:14:17 -0700

    am done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: always 0 for success
+  */
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		printf("%d", d);
+		d++;
+	}
+	printf("\n");
+
+	return (0);
+}
