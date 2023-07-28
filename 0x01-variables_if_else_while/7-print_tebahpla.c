9bcfe6e11af2c3b74e2b2668f588f62a8b9c7f0d
tob-serve <leinaddaniel2800@gmail.com>
2023-07-28 16:32:40 -0700

    am done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * 
+  * Return: Always 0 for success
+  */
+
+int main(void)
+{
+	int d = 122;
+
+	while (d >= 97)
+	{
+		putchar(d);
+		d--;
+	}
+	putchar('\n');
+
+	return (0);
+}
