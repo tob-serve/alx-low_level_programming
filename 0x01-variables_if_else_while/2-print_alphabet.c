03a4dd841dd834f2ba2c395cb37b7b407d2fc464
tob-serve <leinaddaniel2800@gmail.com>
2023-07-28 15:37:44 -0700

    am done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: always 0 for success
+  */
+
+int main(void)
+{
+	int j;
+
+	for (j = 97; j <= 122; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
+		return (0);
+}
