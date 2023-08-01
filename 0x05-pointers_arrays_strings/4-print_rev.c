326dc4785651a9eb2fc26850b33a3c8b5cb05640
tob-serve <leinaddaniel2800@gmail.com>
2023-08-01 15:55:58 -0700

    am done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,35 +9,23 @@
 void print_rev(char *s)
 
 {
+	int longi = 0;
+	int o;
 
-	        int longi = 0;
+	while (*s != '\0')
+	{
+		longi++;
 
-		        int o;
+		s++;
+	}
+	s--;
 
+	for (o = longi; o > 0; o--)
 
-			        while (*s != '\0')
-
-					        {
-
-							                longi++;
-
-									                s++;
-
-											        }
-
-				        s--;
-
-					        for (o = longi; o > 0; o--)
-
-							        {
-
-									                _putchar(*s);
-
-											                s--;
-
-													        }
-
-
-						        _putchar('\n');
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
 
 }
