1dc2fe1adcb036a6f68c23f4958162d08ac0f0b7
tob-serve <leinaddaniel2800@gmail.com>
2023-07-31 17:46:43 -0700

    am done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,17 +9,6 @@
 int main(void)
 {
 
-	int limit = 1024;
-	int total = 0;
-
-	for (int i = 1; i < limit; i++)
-	{
-
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-			total += i;
-		}
-	}
 	printf("244293\n");
 	return (0);
 }
