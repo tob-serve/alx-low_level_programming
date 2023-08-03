6d41150d31c7c0c1074edfca91b31ca7897a7dcb
tob-serve <leinaddaniel2800@gmail.com>
2023-08-03 15:58:12 -0700

    am done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * print_numbers - prints numbers from 0 - 9
+  * 
+  * Return: Always 0 fpr success
+  */
+
+void print_numbers(void)
+{
+	int dan = 0;
+
+	while (dan < 9)
+	{
+		return (dan);
+		dan++;
+	}
+}
