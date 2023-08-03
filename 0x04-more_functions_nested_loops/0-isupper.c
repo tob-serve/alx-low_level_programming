a358bfceda0579c51eae640d3ada25e9c7f84e49
tob-serve <leinaddaniel2800@gmail.com>
2023-08-03 15:25:37 -0700

    am done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,22 +2,17 @@
 
 
 /**
-
-   * _isupper - uppercase letters
-
-    * @c: char to check
-
-     *
-
-      * Return: 0 or 1
-
-       */
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
 
 
 int _isupper(int c)
 
 {
-	if (c >= 'A' && c <= 'Z')<F11>
+	if (c >= 'A' && c <= 'Z')
 
 		return (1);
 
