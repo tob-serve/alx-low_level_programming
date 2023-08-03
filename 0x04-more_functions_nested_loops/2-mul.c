e8b3edd9fb2cbbce9b6d0508c23954848bb8d54e
tob-serve <leinaddaniel2800@gmail.com>
2023-08-03 15:50:09 -0700

    am done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point for program
+  * @a: charcter to multiply
+  * @b: also character to multiply
+  *
+  * Return: Always 0 for success
+  */
+
+int mul(int a, int b)
+{
+	a * b
+}
