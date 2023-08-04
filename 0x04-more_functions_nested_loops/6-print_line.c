747a7f9775b7f0aac8c342870c3c62b987e88272
tob-serve <leinaddaniel2800@gmail.com>
2023-08-04 16:03:41 -0700

    am done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_line - print a line in the terminal
+  */
+
+void print_line(int n)
+{
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
