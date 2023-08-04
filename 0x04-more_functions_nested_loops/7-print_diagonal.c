8eadeda3f88d0c1990c4cd467c01ddfd0a7f88dc
tob-serve <leinaddaniel2800@gmail.com>
2023-08-04 16:32:14 -0700

    am done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_diagonal - draws a diagonal line on screen
+  * @n: number for lin
+  */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			if (j == i)
+				_putchar('\\');
+			else if (j < i)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
