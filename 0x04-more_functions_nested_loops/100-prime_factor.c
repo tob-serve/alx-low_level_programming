ce580f22b70d4da4bb1d894e46d5e67fc0c7c834
tob-serve <leinaddaniel2800@gmail.com>
2023-08-07 01:19:15 -0700

    am done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * main - ENtry point
+  */
+
+int main(void)
+{
+	long int i, n, d;
+
+	n = 612852475143;
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			if (n == i)
+			{
+				printf("%ld\n", i);
+				break;
+			}
+			d = n / i;
+			n = d;
+		}
+	}
+	return (0);
+}
