85389f080c89265e6d3d41f28254addf1477d626
tob-serve <leinaddaniel2800@gmail.com>
2023-08-08 16:26:41 -0700

    am done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,16 +3,11 @@ i
 
 
 /**
-
-   * char *_strcpy - a function that copies the string pointed to by src
-
-    * @dest: copy to
-
-     * @src: copy from
-
-      * Return: string
-
-       */
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
 
 char *_strcpy(char *dest, char *src)
 
