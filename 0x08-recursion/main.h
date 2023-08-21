40c38498ddcc94ee36613e8cbf1664139f2efa03
tob-serve <leinaddaniel2800@gmail.com>
2023-08-21 16:24:49 -0700

    am done

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int _sqrt(int n, int x);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime(int n, int start);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 
