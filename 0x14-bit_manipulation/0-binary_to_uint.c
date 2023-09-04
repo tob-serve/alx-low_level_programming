6a635504fb615ac5d9fec24313e949d533ade646
tob-serve <leinaddaniel2800@gmail.com>
2023-09-04 06:25:26 -0700

    am done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -10,17 +12,32 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec, weight = 1;
-	int rem;
+	unsigned int DecimalValue = 0;
+	int binary = 0, rem = 0, weight = 1;
 
-	while (*b != 0)
+	if (!b)
+		return (0);
+
+	while (*b != '\0')
 	{
-		rem = b % 10;
-		dec = dec + rem*weight;
-		//getting the binary minus the last one we walked on
-		binary = binary / 10;
-		// incrementing weight by 2
-		weight = weight*2;
+		if (*b >= '0' && *b <= '9')
+		{
+			binary = binary * 10 + (*b - '0');
+		}
+		else
+		{
+			break;
+		}
+		b++;
 	}
-	return dec;
+
+	while (binary != 0)
+	{
+		rem = binary % 10;
+		DecimalValue = DecimalValue + rem * weight;
+		binary = binary / 10;
+		weight = weight * 2;
+
+	}
+	return (DecimalValue);
 }
