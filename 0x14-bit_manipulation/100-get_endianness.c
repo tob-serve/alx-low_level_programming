d2500322460f4e4c2fa3a92e94789ada7bc84b84
tob-serve <leinaddaniel2800@gmail.com>
2023-09-06 06:06:33 -0700

    am done

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * get_endianness - check the system byte order
+  * Return: 0 if big endian and 1 if little endian
+  */
+
+int get_endianness(void)
+{
+	int x = 1;
+	int LiBi;
+
+	LiBi = (int) (((char *)&x) [0]);
+	return (LiBi)
+}
