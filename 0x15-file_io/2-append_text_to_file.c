c3d38f683a61489acace57991c9adfc58bb658a3
tob-serve <leinaddaniel2800@mail.com>
2023-09-15 09:27:13 -0700

    am done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (FilePointer == NULL)
 		return (-1);
+
 	if (text_content != NULL)
+	{
 		fprintf(FilePointer, "%s", text_content);
 
 		fclose(FilePointer);
