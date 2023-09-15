0fa2e794dd6c28a41cb2632cb0b29720ae97d7bd
tob-serve <leinaddaniel2800@mail.com>
2023-09-15 08:10:33 -0700

    am done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,18 +20,23 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	FilePointer = fopen(filename, "w");
-
 	if (text_content == NULL)
+	{
+		FilePointer = fopen(filename, "w");
+	}
+	else
+	{
+		FilePointer = fopen(filename, "w+");
+		if (FilePointer != NULL)
+		{
+			fprintf(FilePointer, "%s", text_content);
+		}
+	}
+
+	if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
 	{
 		return (-1);
 	}
-	else if(text_content != NULL)
-	{
-		fprintf(FilePointer, "%s", text_content);
-	}
-
-	chmod(filename, S_IRUSR | S_IWUSR);
 
 	fclose(FilePointer);
 
