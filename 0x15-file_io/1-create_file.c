4cd39ec91ec6c1ac2afa2664e134ef3933d56954
tob-serve <leinaddaniel2800@mail.com>
2023-09-15 07:44:27 -0700

    am done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,10 @@
 int create_file(const char *filename, char *text_content)
 {
 	FILE *FilePointer;
+	struct stat filestat;
+
+	if (stat(filename, &filestat) == 0)
+		remove(filename);
 
 	if (text_content == NULL)
 	{
