0e95b2763bdea125dce180516fb344e4079964a9
tob-serve <leinaddaniel2800@mail.com>
2023-09-15 09:16:30 -0700

    am done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,10 +14,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	FILE *FilePointer;
 	struct stat filestat;
 
-	if (stat(filename, &filestat) != 0)
+	if (filename == NULL)
 		return (-1);
 
-	if (filename == NULL)
+	if (stat(filename, &filestat) != 0)
 		return (-1);
 
 	if (stat(filename, &filestat) == 0)
@@ -27,9 +27,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		{
 			fprintf(FilePointer, "%s", text_content);
 		}
-	}
 
-	fclose(FilePointer);
+		fclose(FilePointer);
+	}
 
 	return (1);
 }
