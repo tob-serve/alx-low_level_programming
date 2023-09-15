d9692183c01ee243d0fe82625babdbb65d1703cf
tob-serve <leinaddaniel2800@mail.com>
2023-09-15 07:12:50 -0700

    am done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,14 @@ int create_file(const char *filename, char *text_content)
 {
 	FILE *FilePointer;
 
-	if (filename == NULL || text_content == NULL)
-		return (0);
+	if (text_content != NULL)
+		FilePointer = fopen(filename, "w");
 
-	FilePointer = fopen(filename, "w");
-
-	fprintf(FilePointer, "%s", text_content);
+	else
+	{
+		FilePointer = fopen(filename, "w");
+		fprintf(FilePointer, "%s", text_content);
+	}
 
 	if (filename == NULL)
 		return (-1);
