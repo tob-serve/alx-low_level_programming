d0a92098c2642469754947f64546ea381835d242
tob-serve <leinaddaniel2800@mail.com>
2023-09-20 14:44:13 -0700

    cheking code changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,122 +1,98 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
-  * file_toCheck - checks if file_to exist
-  * it does its content gets cleard for new input
+  * file_fromCheck - checks if file_to exist if
+  * it does truncate
   *
-  * @SecArgv: name of to copy to
+  * @argv: second argument vector file name
+  *
+  * Return: 0 for sucess
   */
 
-void file_toCheck(char *SecArgv)
-{
-	struct stat filestat;
-	FILE *FilePointer;
-	int FileDes = 0;
-
-	if (stat(SecArgv, &filestat) == 0)
-	{
-		FilePointer = fopen(SecArgv, "w");
-		fclose(FilePointer);
-	}
-	if (stat(SecArgv, &filestat) != 0)
-	{
-		FileDes = open(SecArgv,
-				O_WRONLY | O_CREAT,
-				S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	}
-	close(FileDes);
-}
-
-/**
-  * file_fromCheck -  checks if file_from exist if it dont
-  * return code 98
-  *
-  * @FirstArgv: first agrgument vector
-  */
-
-void file_fromCheck(char *FirstArgv)
+int file_fromCheck(char *argv)
 {
 	struct stat filestat;
 
-	if (stat(FirstArgv, &filestat) != 0)
+	if (stat(argv, &filestat) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", FirstArgv);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
 		exit(98);
 	}
+
+	return (0);
 }
 
 /**
-  * Copyfile_fromfile_to - copys file_from to _file_to
+  * CopyFiles - copy files from one to another
   *
-  * @FirstArgv: first file name
-  * @SecArgv: second file name
+  * @argvOne: first file name
+  * @argvTwo: second file name
+  *
+  * Return: 0 for success
   */
 
-void Copyfile_fromfile_to(char *FirstArgv, char *SecArgv)
+int CopyFiles(char *argvOne, char *argvTwo)
 {
-	int FileDesOne = -1, FileDesTwo = -1;
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
 	char buffer[1024];
-	ssize_t BytesRead, BytesWritten;
 
-	FileDesOne = open(FirstArgv, O_RDONLY);
-	if (FileDesOne == -1)
+	fd_from = open(argvOne, O_RDONLY);
+	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", FirstArgv);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argvOne);
 		exit(98);
 	}
-
-	FileDesTwo = open(SecArgv, O_RDWR);
-	if (FileDesTwo == -1)
+	fd_to = open(argvTwo, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", SecArgv);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argvTwo);
 		exit(99);
 	}
-	while ((BytesRead = read(FileDesOne, buffer, sizeof(buffer))) > 0)
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
-		BytesWritten = write(FileDesTwo, buffer, BytesRead);
-
-		if  (BytesWritten != BytesRead)
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", SecArgv);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argvTwo);
 			exit(99);
 		}
 	}
-	if (close(FileDesOne) == -1)
+	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n", FileDesOne);
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fd_to);
 		exit(100);
 	}
-	if (close(FileDesTwo) == -1)
+	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n", FileDesTwo);
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fd_to);
 		exit(100);
 	}
+	return (0);
+
+
 }
 
 /**
   * main - Entry point of program
   *
-  * @argc: argument count
+  * @argc: argu,ent count
   * @argv: argument vector
   *
-  * Return: 0 for success
+  * Return: always 0 for success
   */
 
 int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	file_toCheck(argv[2]);
-
 	file_fromCheck(argv[1]);
 
-	Copyfile_fromfile_to(argv[1], argv[2]);
+	CopyFiles(argv[1], argv[2]);
 
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,9 +15,8 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
-void file_toCheck(char *SecArgv);
-void file_fromCheck(char *FirstArgv);
-void Copyfile_fromfile_to(char *FirstArgv, char *SecArgv);
+int file_fromCheck(char *argv);
+int CopyFiles(char *argvOne, char *argvTwo);
 int main(int argc, char **argv);
 
 #endif
